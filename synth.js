812d96c5aeabbef51083059e08bd4a9330e032d3
Shadetrap <mikkeluml@gmail.com>
2017-12-11 16:47:57 +0100

    Create synth.js

---
+++ synth.js
@@ -0,0 +1,32 @@
+
+body
+{
+    font-family:Arial;
+}
+.wrapper {
+
+}
+
+
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    /*padding-right:100px;*/
+}
+#test{
+    background:orange;
+}
+table tr td:first-child {
+    padding-right:20px;
+}
+a {
+    color:darkblue;
+}
+a:hover {
+    color: deepskyblue;
+    cursor: default;
+}
+.playlist{
+    width: 650px;
+    margin: 0 auto;
+}
